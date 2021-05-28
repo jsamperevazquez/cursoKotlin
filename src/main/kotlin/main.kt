42b1fc0f3964918c54798b4c0d5f4d62daf15259
Jose Ángel <jsamperevazquez@danielcastelao.org>
2021-05-28 22:45:53 +0200

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+
+    val numero=10   // Tipo val no se puede reasignar (final)
+
+    var numero2 =20 // var se puede modificar o reasignar
+    numero2 = 30
+    println(numero)
+    println(numero2)
+
+    
+}
