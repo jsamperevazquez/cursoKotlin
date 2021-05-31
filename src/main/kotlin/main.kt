ad8fe1c35f524f04b3ae21a68cc57733baebdf88
Jose Ángel <jsamperevazquez@danielcastelao.org>
2021-06-01 01:06:30 +0200

    Petición de datos al usuario con readline, concatenar y rawString

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -7,5 +7,27 @@ fun main(args: Array<String>) {
     println(numero)
     println(numero2)
 
-    
+    // Petición de datos al usuario
+    println("Introduce tu nombre: ")
+    val nombre= readLine()
+
+    println("Introduce tu edad: ")
+    val edad= readLine()
+
+    // primera manera de concatenar
+    println("Tu nombre es: " + nombre + " y tu edad es de " + edad + " annos")
+
+    // Segunda manera de concatenar más práctica (String template)
+    println("Tu nombre es: $nombre y tu edad es de $edad annos")
+
+
+    //Raw String (tres veces comillas dobles y enter ya nos da el método para ello)
+    println("""
+        *** DATOS ***
+        nombre: $nombre
+        edad: $edad annos
+    """.trimIndent())
+
+
+
 }
