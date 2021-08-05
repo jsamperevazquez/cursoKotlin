a1be827b551ac8c7912e71d08d6724b3d687f10e
jsamperevazquez <jsamperevazquez@danielcastelao.org>
2021-08-05 18:12:09 +0200

    data class (almacenar datos)

---
+++ src/main/kotlin/dataClass/Persona.kt
@@ -0,0 +1,8 @@
+package dataClass
+
+data class Persona(var nombre: String, var telefono: String)
+
+
+// Diferencia con las clases normales
+class  PersonaB( var nombre: String, var telefono: String)
+
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,19 +1,5 @@
-import arrayList.ArrayListOf
-import cicloFor.For
-import clasesAbstractas.Rectangulo
-import ejerciciosIniciales.Ejercicio4
-import ejerciciosIniciales.Ejercicio5
-import ejerciciosIniciales.Ejercicio6
-import ejerciciosIniciales.Ejercicio7
-import excepciones.Throw
-import excepciones.TryCatch
-import herencia.Empleado
-import herencia.Operario
-import herencia.Persona
-import interfaces.Cuadrado
-import nullSafety.Nulos
-import whileDoWhile.DoWhile
-import whileDoWhile.While
+import dataClass.Persona
+import dataClass.PersonaB
 
 fun main(args: Array<String>) {
 /*
@@ -53,10 +39,9 @@ fun main(args: Array<String>) {
     println(Funciones().ejemploPred())
 
     println(Funciones().calcularIva(producto = 289.00))
+    */
 
- */
-
-      //  TasaCambio().calcularCambio()
+    //  TasaCambio().calcularCambio()
     // Ejercicio1().solicitarDatos()
     // Ejercicio2().calcularNeto()
 
@@ -65,11 +50,10 @@ fun main(args: Array<String>) {
     Ejercicio3().calcularArea()
     Ejercicio3().calcularPerimetro()
     Ejercicio3().visualizarDatos()
-
      */
 
     //  When().usarWhen()
-     // When().comprobarCombustible()
+    // When().comprobarCombustible()
     // When().otraManeraWhen()
     // When().whenEnRango()
     // When().whenEnVariable()
@@ -81,21 +65,23 @@ fun main(args: Array<String>) {
     // While().pedirEmpleados()
     // DoWhile().pedirNombres()
     // Ejercicio4().calcularPrecioTrayecto()
-   /* Ejercicio5().calcularPromedio()
-    Ejercicio5().calificacionMasAlta()
-    Ejercicio5().calificacionMasBaja()
-    Ejercicio5().alumnosReprobados()
-    Ejercicio6().sumarListaNumeros()
 
-    */
+    /* Ejercicio5().calcularPromedio()
+     Ejercicio5().calificacionMasAlta()
+     Ejercicio5().calificacionMasBaja()
+     Ejercicio5().alumnosReprobados()
+     Ejercicio6().sumarListaNumeros()
+     */
     //TryCatch().capturarExcpcion()
     // TryCatch().capturarExcepcionDesdeArchivo()
     // TryCatch().esNumero("4")
     // println(Throw().calcularInteresPrestamo(readLine()!!.toDouble()))
-   /* Nulos().calcularAreaCuadrado()
+
+    /* Nulos().calcularAreaCuadrado()
     Nulos().calcularAreaCuadradoConElvis()
     Nulos().calcularAreaCuadradoOtraOpcion()
     Nulos().calcularAreaCuadradoLLamadaSegura()
+
     */
     // Ejercicio7().calcularDonas()
     /*
@@ -108,8 +94,8 @@ fun main(args: Array<String>) {
     val operario1 = Operario("Marcos", "Martinez", "Orense 13","986452432", "30/11/1981",2000.0 , "Oficial")
     println(operario1.obtenerEdad())
     println(operario1)
-
      */
+
     /*
     var rectangulo = Rectangulo(10.0, 5.0,"Rectangulo 1")
     println(rectangulo.obtenerArea())
@@ -117,11 +103,36 @@ fun main(args: Array<String>) {
     println(rectangulo.obtenerAreaPulgadas())
     println(rectangulo.obtenerPerimetroPulgadas())
     rectangulo.dibujar()
-
      */
+
+    /*
     var cuadrado1 = Cuadrado(8.0,5.0,"cuadrado")
     println(cuadrado1.obtenerArea())
     println(cuadrado1.obtenerPerimetro())
     cuadrado1.dibujar()
+     */
+    /*
+    // Las data class se utilizan para almacenar datos y atributos de objetos:
+    val persona1 = Persona("Angel" , "986345432")
+    println("La data class ya viene implementado el toString ::: $persona1")
+
+    val persona2 = PersonaB("Juan", "678989878")
+    println("La clase normal sin toString te muestra la dirección de memoria ::: $persona2")
+
+    // Para hacer copia de un objeto con data class de forma directa (Todos los atributos del objeto):
+    val persona3 = persona1.copy()
+    println(persona3)
+
+    // Para copiar solo atributos que deseemos copiar:
+    val persona4 = persona1.copy("Marcos", persona1.telefono)
+    println(persona4)
+
+    // Las data class permiten desestructurar un objeto:
+    val (nombre,telefono) = persona1
+    println("$nombre,$telefono")
+     */
+
+
+
 
 }
